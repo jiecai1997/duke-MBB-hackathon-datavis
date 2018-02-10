7c2d58e90cf81cd2659739471c7e191715803636
jie.cai2 <jc561@duke.edu>
2018-02-10 15:40:25 -0500

    .gitignore is now working

--- sql/test.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT  d.opponent, d.headcoach, d.oppcoach
-FROM duke_data_2018 d, opponent_data_2018 o
-WHERE d.gameid_game_info = o.gameid_game_info
-AND d.coach = "Mike Krzyzewski"
-AND d.oppcoachid = o.oppcoachid
-AND d.oppcoach != ""
-ORDER BY d.season4;
--- sql/test3.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-
-FROM duke_2018_sorted
