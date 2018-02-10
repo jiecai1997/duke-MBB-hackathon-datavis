014bbb0e1c61f626aa3b260f5b3b0584a3ed6c29
jie.cai2 <jc561@duke.edu>
2018-02-10 02:09:09 -0500

    added more historical data, need to sort by opponentid instead of gameid

---
+++ sql/opp_clean_1y.sql
@@ -0,0 +1,41 @@
+-- execute below SQL on CSV files using queryscsv.py
+
+SELECT
+  season,
+  gameid,
+  opponent,
+  round(avg(tp),2) as tp,
+  round(avg(fg),2) as fg,
+  round(avg(fg3),2) as fg3,
+  round(avg(ft),2) as ft,
+  round(avg(oreb),2) as oreb,
+  round(avg(dreb),2) as dreb,
+  round(avg(ast),2) as ast,
+  round(avg(blk),2) as blk,
+  round(avg(stl),2) as stl
+
+  FROM
+  (SELECT
+    cast(REPLACE(season4, ',', '') as int) as season,
+    cast(REPLACE(gameid, ',', '') as int) as gameid,
+    opponent,
+    sum(tp) as tp,
+    sum(fg) as fg,
+    sum(fg3) as fg3,
+    sum(ft) as ft,
+    sum(oreb) as oreb,
+    sum(dreb) as dreb,
+    sum(ast) as ast,
+    sum(blk) as blk,
+    sum(stl) as stl
+
+  FROM
+    opp_data_2018
+
+  WHERE
+    cast(REPLACE(season4, ',', '') as int) = 2018
+
+  GROUP BY gameid)
+
+GROUP BY opponent
+ORDER BY season;
