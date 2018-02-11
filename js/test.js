fde1e519c6527c075dd3ed95f95df8b2c21c66fc
jiecai1997 <jiecai1997@gmail.com>
2018-02-11 10:52:44 -0500

    fixed html

---
+++ js/test.js
@@ -0,0 +1,620 @@
+<script type="text/javascript">
+
+	function getData() {
+
+		let allData =
+		{
+  "duke-stats" : [ {
+    "ast" : 21,
+    "blk" : 3,
+    "fg" : 41,
+    "fg3" : 12,
+    "ft" : 3,
+    "gameid" : "20,171,110",
+    "opponent" : "Elon",
+    "pf" : 16,
+    "stl" : 7,
+    "tp" : 97,
+    "treb" : 45
+  }, {
+    "ast" : 26,
+    "blk" : 7,
+    "fg" : 39,
+    "fg3" : 10,
+    "ft" : 11,
+    "gameid" : "20,171,111",
+    "opponent" : "Utah Valley",
+    "pf" : 16,
+    "stl" : 14,
+    "tp" : 99,
+    "treb" : 40
+  }, {
+    "ast" : 21,
+    "blk" : 5,
+    "fg" : 30,
+    "fg3" : 9,
+    "ft" : 19,
+    "gameid" : "20,171,114",
+    "opponent" : "Michigan State",
+    "pf" : 20,
+    "stl" : 12,
+    "tp" : 88,
+    "treb" : 42
+  }, {
+    "ast" : 14,
+    "blk" : 10,
+    "fg" : 25,
+    "fg3" : 4,
+    "ft" : 24,
+    "gameid" : "20,171,117",
+    "opponent" : "Southern",
+    "pf" : 15,
+    "stl" : 3,
+    "tp" : 78,
+    "treb" : 47
+  }, {
+    "ast" : 18,
+    "blk" : 8,
+    "fg" : 39,
+    "fg3" : 5,
+    "ft" : 9,
+    "gameid" : "20,171,120",
+    "opponent" : "Furman",
+    "pf" : 9,
+    "stl" : 9,
+    "tp" : 92,
+    "treb" : 38
+  }, {
+    "ast" : 19,
+    "blk" : 9,
+    "fg" : 32,
+    "fg3" : 4,
+    "ft" : 31,
+    "gameid" : "20,171,123",
+    "opponent" : "Portland State",
+    "pf" : 16,
+    "stl" : 2,
+    "tp" : 99,
+    "treb" : 45
+  }, {
+    "ast" : 21,
+    "blk" : 3,
+    "fg" : 30,
+    "fg3" : 3,
+    "ft" : 22,
+    "gameid" : "20,171,124",
+    "opponent" : "Texas",
+    "pf" : 20,
+    "stl" : 8,
+    "tp" : 85,
+    "treb" : 46
+  }, {
+    "ast" : 20,
+    "blk" : 4,
+    "fg" : 29,
+    "fg3" : 10,
+    "ft" : 19,
+    "gameid" : "20,171,126",
+    "opponent" : "Florida",
+    "pf" : 18,
+    "stl" : 7,
+    "tp" : 87,
+    "treb" : 38
+  }, {
+    "ast" : 15,
+    "blk" : 3,
+    "fg" : 33,
+    "fg3" : 3,
+    "ft" : 22,
+    "gameid" : "20,171,129",
+    "opponent" : "Indiana",
+    "pf" : 22,
+    "stl" : 7,
+    "tp" : 91,
+    "treb" : 30
+  }, {
+    "ast" : 17,
+    "blk" : 6,
+    "fg" : 35,
+    "fg3" : 7,
+    "ft" : 19,
+    "gameid" : "20,171,202",
+    "opponent" : "South Dakota",
+    "pf" : 18,
+    "stl" : 4,
+    "tp" : 96,
+    "treb" : 47
+  }, {
+    "ast" : 34,
+    "blk" : 4,
+    "fg" : 48,
+    "fg3" : 15,
+    "ft" : 13,
+    "gameid" : "20,171,205",
+    "opponent" : "Saint Francis (Pa.)",
+    "pf" : 16,
+    "stl" : 3,
+    "tp" : 124,
+    "treb" : 60
+  }, {
+    "ast" : 13,
+    "blk" : 3,
+    "fg" : 32,
+    "fg3" : 8,
+    "ft" : 12,
+    "gameid" : "20,171,209",
+    "opponent" : "Boston College",
+    "pf" : 20,
+    "stl" : 10,
+    "tp" : 84,
+    "treb" : 31
+  }, {
+    "ast" : 32,
+    "blk" : 9,
+    "fg" : 39,
+    "fg3" : 16,
+    "ft" : 10,
+    "gameid" : "20,171,220",
+    "opponent" : "Evansville",
+    "pf" : 13,
+    "stl" : 13,
+    "tp" : 104,
+    "treb" : 33
+  }, {
+    "ast" : 19,
+    "blk" : 5,
+    "fg" : 39,
+    "fg3" : 8,
+    "ft" : 14,
+    "gameid" : "20,171,230",
+    "opponent" : "Florida State",
+    "pf" : 21,
+    "stl" : 6,
+    "tp" : 100,
+    "treb" : 51
+  }, {
+    "ast" : 10,
+    "blk" : 5,
+    "fg" : 31,
+    "fg3" : 3,
+    "ft" : 20,
+    "gameid" : "20,180,106",
+    "opponent" : "N.C. State",
+    "pf" : 21,
+    "stl" : 7,
+    "tp" : 85,
+    "treb" : 28
+  }, {
+    "ast" : 21,
+    "blk" : 5,
+    "fg" : 34,
+    "fg3" : 12,
+    "ft" : 7,
+    "gameid" : "20,180,110",
+    "opponent" : "Pittsburgh",
+    "pf" : 18,
+    "stl" : 11,
+    "tp" : 87,
+    "treb" : 38
+  }, {
+    "ast" : 18,
+    "blk" : 5,
+    "fg" : 25,
+    "fg3" : 12,
+    "ft" : 27,
+    "gameid" : "20,180,113",
+    "opponent" : "Wake Forest",
+    "pf" : 15,
+    "stl" : 10,
+    "tp" : 89,
+    "treb" : 42
+  }, {
+    "ast" : 18,
+    "blk" : 5,
+    "fg" : 31,
+    "fg3" : 11,
+    "ft" : 10,
+    "gameid" : "20,180,115",
+    "opponent" : "Miami",
+    "pf" : 10,
+    "stl" : 8,
+    "tp" : 83,
+    "treb" : 41
+  }, {
+    "ast" : 16,
+    "blk" : 7,
+    "fg" : 29,
+    "fg3" : 12,
+    "ft" : 11,
+    "gameid" : "20,180,120",
+    "opponent" : "Pittsburgh",
+    "pf" : 14,
+    "stl" : 8,
+    "tp" : 81,
+    "treb" : 33
+  }, {
+    "ast" : 12,
+    "blk" : 2,
+    "fg" : 24,
+    "fg3" : 6,
+    "ft" : 30,
+    "gameid" : "20,180,123",
+    "opponent" : "Wake Forest",
+    "pf" : 17,
+    "stl" : 11,
+    "tp" : 84,
+    "treb" : 31
+  }, {
+    "ast" : 15,
+    "blk" : 5,
+    "fg" : 27,
+    "fg3" : 4,
+    "ft" : 5,
+    "gameid" : "20,180,127",
+    "opponent" : "Virginia",
+    "pf" : 14,
+    "stl" : 2,
+    "tp" : 63,
+    "treb" : 42
+  }, {
+    "ast" : 15,
+    "blk" : 6,
+    "fg" : 32,
+    "fg3" : 12,
+    "ft" : 12,
+    "gameid" : "20,180,129",
+    "opponent" : "Notre Dame",
+    "pf" : 14,
+    "stl" : 5,
+    "tp" : 88,
+    "treb" : 41
+  } ],
+  "opp-stats" : [ {  // OPPONENT DATA STARTS HERE
+    "ast" : 10,
+    "blk" : 1,
+    "fg" : 25,
+    "fg3" : 14,
+    "ft" : 4,
+    "gameid" : "20,171,110",
+    "opponent" : "Elon",
+    "pf" : 12,
+    "stl" : 3,
+    "treb" : 28
+  }, {
+    "ast" : 13,
+    "blk" : 0,
+    "fg" : 25,
+    "fg3" : 6,
+    "ft" : 13,
+    "gameid" : "20,171,111",
+    "opponent" : "Utah Valley",
+    "pf" : 15,
+    "stl" : 5,
+    "treb" : 33
+  }, {
+    "ast" : 25,
+    "blk" : 12,
+    "fg" : 31,
+    "fg3" : 9,
+    "ft" : 10,
+    "gameid" : "20,171,114",
+    "opponent" : "Michigan State",
+    "pf" : 21,
+    "stl" : 1,
+    "treb" : 30
+  }, {
+    "ast" : 8,
+    "blk" : 4,
+    "fg" : 22,
+    "fg3" : 7,
+    "ft" : 10,
+    "gameid" : "20,171,117",
+    "opponent" : "Southern",
+    "pf" : 27,
+    "stl" : 5,
+    "treb" : 32
+  }, {
+    "ast" : 14,
+    "blk" : 0,
+    "fg" : 25,
+    "fg3" : 9,
+    "ft" : 4,
+    "gameid" : "20,171,120",
+    "opponent" : "Furman",
+    "pf" : 15,
+    "stl" : 7,
+    "treb" : 25
+  }, {
+    "ast" : 21,
+    "blk" : 1,
+    "fg" : 30,
+    "fg3" : 12,
+    "ft" : 9,
+    "gameid" : "20,171,123",
+    "opponent" : "Portland State",
+    "pf" : 33,
+    "stl" : 6,
+    "treb" : 31
+  }, {
+    "ast" : 14,
+    "blk" : 3,
+    "fg" : 30,
+    "fg3" : 4,
+    "ft" : 14,
+    "gameid" : "20,171,124",
+    "opponent" : "Texas",
+    "pf" : 26,
+    "stl" : 4,
+    "treb" : 33
+  }, {
+    "ast" : 11,
+    "blk" : 2,
+    "fg" : 30,
+    "fg3" : 8,
+    "ft" : 16,
+    "gameid" : "20,171,126",
+    "opponent" : "Florida",
+    "pf" : 17,
+    "stl" : 7,
+    "treb" : 31
+  }, {
+    "ast" : 12,
+    "blk" : 4,
+    "fg" : 30,
+    "fg3" : 5,
+    "ft" : 16,
+    "gameid" : "20,171,129",
+    "opponent" : "Indiana",
+    "pf" : 26,
+    "stl" : 4,
+    "treb" : 28
+  }, {
+    "ast" : 18,
+    "blk" : 0,
+    "fg" : 30,
+    "fg3" : 10,
+    "ft" : 10,
+    "gameid" : "20,171,202",
+    "opponent" : "South Dakota",
+    "pf" : 24,
+    "stl" : 6,
+    "treb" : 21
+  }, {
+    "ast" : 11,
+    "blk" : 0,
+    "fg" : 24,
+    "fg3" : 7,
+    "ft" : 12,
+    "gameid" : "20,171,205",
+    "opponent" : "Saint Francis (Pa.)",
+    "pf" : 18,
+    "stl" : 8,
+    "treb" : 22
+  }, {
+    "ast" : 18,
+    "blk" : 1,
+    "fg" : 31,
+    "fg3" : 15,
+    "ft" : 12,
+    "gameid" : "20,171,209",
+    "opponent" : "Boston College",
+    "pf" : 18,
+    "stl" : 3,
+    "treb" : 31
+  }, {
+    "ast" : 11,
+    "blk" : 2,
+    "fg" : 15,
+    "fg3" : 5,
+    "ft" : 5,
+    "gameid" : "20,171,220",
+    "opponent" : "Evansville",
+    "pf" : 14,
+    "stl" : 1,
+    "treb" : 17
+  }, {
+    "ast" : 22,
+    "blk" : 2,
+    "fg" : 31,
+    "fg3" : 15,
+    "ft" : 16,
+    "gameid" : "20,171,230",
+    "opponent" : "Florida State",
+    "pf" : 22,
+    "stl" : 3,
+    "treb" : 33
+  }, {
+    "ast" : 17,
+    "blk" : 3,
+    "fg" : 34,
+    "fg3" : 5,
+    "ft" : 23,
+    "gameid" : "20,180,106",
+    "opponent" : "N.C. State",
+    "pf" : 19,
+    "stl" : 7,
+    "treb" : 30
+  }, {
+    "ast" : 8,
+    "blk" : 5,
+    "fg" : 17,
+    "fg3" : 4,
+    "ft" : 14,
+    "gameid" : "20,180,110",
+    "opponent" : "Pittsburgh",
+    "pf" : 14,
+    "stl" : 3,
+    "treb" : 27
+  }, {
+    "ast" : 15,
+    "blk" : 4,
+    "fg" : 27,
+    "fg3" : 10,
+    "ft" : 7,
+    "gameid" : "20,180,113",
+    "opponent" : "Wake Forest",
+    "pf" : 26,
+    "stl" : 5,
+    "treb" : 35
+  }, {
+    "ast" : 10,
+    "blk" : 2,
+    "fg" : 29,
+    "fg3" : 9,
+    "ft" : 8,
+    "gameid" : "20,180,115",
+    "opponent" : "Miami",
+    "pf" : 16,
+    "stl" : 13,
+    "treb" : 36
+  }, {
+    "ast" : 7,
+    "blk" : 3,
+    "fg" : 20,
+    "fg3" : 4,
+    "ft" : 10,
+    "gameid" : "20,180,120",
+    "opponent" : "Pittsburgh",
+    "pf" : 14,
+    "stl" : 6,
+    "treb" : 23
+  }, {
+    "ast" : 11,
+    "blk" : 1,
+    "fg" : 27,
+    "fg3" : 5,
+    "ft" : 11,
+    "gameid" : "20,180,123",
+    "opponent" : "Wake Forest",
+    "pf" : 28,
+    "stl" : 1,
+    "treb" : 35
+  }, {
+    "ast" : 12,
+    "blk" : 3,
+    "fg" : 26,
+    "fg3" : 6,
+    "ft" : 7,
+    "gameid" : "20,180,127",
+    "opponent" : "Virginia",
+    "pf" : 18,
+    "stl" : 8,
+    "treb" : 27
+  }, {
+    "ast" : 13,
+    "blk" : 1,
+    "fg" : 22,
+    "fg3" : 7,
+    "ft" : 15,
+    "gameid" : "20,180,129",
+    "opponent" : "Notre Dame",
+    "pf" : 12,
+    "stl" : 8,
+    "treb" : 25
+  } ]
+};
+
+// need clean file
+/* $.getJSON('contacts.json', function (json) {
+var array = [];
+for (var key in json) {
+    if (json.hasOwnProperty(key)) {
+        var item = json[key];
+        array.push({
+            asissts: item.ast,
+
+        });
+    }
+}
+});
+*/
+
+function usersIput(){
+	var users = document.getElementbyId("users").value;
+}
+
+//	var oppTeam = "Notre Dame"; // custom input
+	var oppTeam = prompt("Type in an opponent");
+	var parsedData = [ ["Duke"], [oppTeam] ];
+
+	Object.keys(allData).map(function(mainData, index) {
+		let currentArr = allData[mainData];
+		for(var i in currentArr) {
+			var currentGame = currentArr[i];
+			Object.keys(currentGame).map(function(key, j) {
+				if(currentGame[key] === oppTeam) {
+					parsedData[index].push(currentGame["ast"]);
+					parsedData[index].push(currentGame["blk"]);
+					parsedData[index].push(currentGame["fg"]);
+					parsedData[index].push(currentGame["fg3"]);
+					parsedData[index].push(currentGame["ft"]);
+					parsedData[index].push(currentGame["pf"]);
+					parsedData[index].push(currentGame["stl"]);
+					parsedData[index].push(currentGame["treb"]);
+
+				}
+			});
+		}
+	});
+// "Total Points", "Field Goals", "3 Point Field Goal", "Free Throws", "Offensive Rebounds", "Defensive Rebounds",
+// 		 	 "Blocks", "Steals",
+
+		let data = [
+		 	["Name", "Asissts", "Blocks", "Field Goals", "3 Pointers", "Free Throws", "Personal Fouls", "Steals", "Total Rebounds"],
+
+		 	parsedData[0],
+		 	parsedData[1],
+		  ]
+		return data;
+	}
+
+
+  function showRadar(){
+    var data = [];
+    var chart = RadarChart.chart();
+
+  let width = 600;
+  let height = 600;
+
+  var csv = getData();
+  headers = []
+  csv.forEach(function(item, i){
+    if(i==0){
+      headers = item;
+    }else{
+      newSeries = {};
+      item.forEach( function(v, j){
+        if(j==0){
+          newSeries.className = v;
+          newSeries.axes = [];
+        }else{
+          newSeries.axes.push({"axis":[headers[j]], "value": parseFloat(v)});
+        }
+      });
+      data.push(newSeries);
+    }
+  })
+
+
+console.log(csv);
+RadarChart.defaultConfig.radius = 3;
+RadarChart.defaultConfig.w = width;
+RadarChart.defaultConfig.h = height;
+RadarChart.draw("#chart-container", data);
+function animate(elem,time) {
+    if( !elem) return;
+    var to = elem.offsetTop;
+    var from = window.scrollY;
+    var start = new Date().getTime(),
+        timer = setInterval(function() {
+            var step = Math.min(1,(new Date().getTime()-start)/time);
+            window.scrollTo(0,(from+step*(to-from))+1);
+            if( step == 1){ clearInterval(timer);};
+        },25);
+        window.scrollTo(0,(from+1));
+    }
+
+var divVal = document.getElementById('chart-container');
+animate(divVal,600);
+}
+</script>
